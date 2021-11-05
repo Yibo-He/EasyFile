1daf482a228cb7f655fead8af59f2e54186c88ce
kite99520 <61300632+kite99520@users.noreply.github.com>
2021-11-05 22:14:35 +0800

    add CORS to connect frontend

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask, blueprints
+from flask_cors import CORS
 
 def create_app(test_config = None):
 
     app = Flask(__name__, instance_relative_config=True)
+    CORS(app, supports_credentials=True)
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=False)
